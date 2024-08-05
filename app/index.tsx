4b9c98ec8859271bae6a63818e0e1e0cc7793da2
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-08-05 18:43:09 +0530

    chore: Add NativeWind plugin to Babel configuration and update npm dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
 import React from 'react'
+import beachImage from "@/assets/meditation-images/beach.webp"
+import { LinearGradient } from 'expo-linear-gradient'
+import { StatusBar } from 'expo-status-bar'
+import { styled } from 'nativewind'
 
-const index = () => {
+const StyledView = styled(View)
+const StyledText = styled(Text)
+const StyledImageBackground = styled(ImageBackground)
+const StyledSafeAreaView = styled(SafeAreaView)
+const StyledLinearGradient = styled(LinearGradient)
+
+const App = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <StyledView className='flex-1'>
+      <StyledImageBackground 
+        source={beachImage} 
+        className='flex-1'
+        resizeMode='cover'
+      >
+        <StyledLinearGradient
+          className='flex-1' 
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <StyledSafeAreaView className='flex-1'>
+            <StyledView className='flex-1 px-1 mt-10'>
+              <StyledText className='text-4xl font-bold text-center text-white'>
+                Simple Meditation
+              </StyledText>
+              <StyledText className='mt-3 text-2xl font-bold text-center text-white '>
+                Simplifying Meditation for Everyone
+              </StyledText>
+            </StyledView>
+            <StatusBar style='light' />
+          </StyledSafeAreaView>
+        </StyledLinearGradient>
+      </StyledImageBackground>
+    </StyledView>
   )
 }
 
-export default index
+export default App
